658a22965adffbebd5b3306e5abe399119eba1cc
Adam Wendt <adam@awendtconsulting.com>
2023-04-09 12:45:28 -0700

    teapot demo / no texture

---
+++ src/main.rs
@@ -0,0 +1,276 @@
+extern crate glium;
+extern crate image;
+
+mod teapot;
+
+use glium::{glutin, implement_vertex, uniform, Surface};
+use std::io::Cursor;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2],
+}
+
+implement_vertex!(Vertex, position, tex_coords);
+
+fn load_image<'a>() -> glium::texture::RawImage2d<'a, u8> {
+    let image = image::load(
+        Cursor::new(&include_bytes!("..\\assets\\textures\\uv-test-bw.jpg")),
+        image::ImageFormat::Jpeg,
+    )
+    .unwrap()
+    .to_rgba8();
+    let image_dimensions = image.dimensions();
+
+    glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions)
+}
+
+fn main() {
+    // 1. The **winit::EventsLoop** for handling events.
+    let event_loop = glutin::event_loop::EventLoop::new();
+
+    // 2. Parameters for building the Window.
+    let wb = glutin::window::WindowBuilder::new()
+        .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello world!");
+
+    // 3. Parameters for building the OpenGL context.
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
+
+    // 4. Build the Display with the given window and OpenGL context parameters
+    //    and register the window with the event_loop.
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let program = get_shader_program(&display);
+    // textures
+    let texture = glium::texture::SrgbTexture2d::new(&display, load_image()).unwrap();
+
+    // Prepare vertices for drawing.
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+        tex_coords: [0.0, 0.0],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+        tex_coords: [0.0, 1.0],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+        tex_coords: [1.0, 0.0],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+    // Teapot
+    let teapot_positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let teapot_normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let teapot_indices = glium::IndexBuffer::new(
+        &display,
+        glium::index::PrimitiveType::TrianglesList,
+        &teapot::INDICES,
+    )
+    .unwrap();
+
+    // Variables to update each frame
+    let mut t: f32 = -0.5;
+
+    // Loop forever until we receive `CloseRequested` event.
+    event_loop.run(move |ev, _, control_flow| {
+        let frame_start_time = std::time::Instant::now();
+
+        // Check to see if we should exit before doing work
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    // Request to exit
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            // Wait until we are ready to draw again
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => (),
+        }
+
+        // Update `t`
+        t += 0.0002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let params = get_draw_parameters();
+
+        // Draw stuff!
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.1, 0.7, 0.5, 1.0), 1.0);
+        let uniforms = get_uniforms(&target);
+
+        // Draw the teacup
+        target
+            .draw(
+                (&teapot_positions, &teapot_normals),
+                &teapot_indices,
+                &program,
+                &uniforms,
+                &params,
+            )
+            .unwrap();
+
+        target.finish().unwrap();
+        // End draw stuff...
+
+        // Attempt to display 60 frames per second.
+        let next_frame_time = frame_start_time + std::time::Duration::from_nanos(16_666_667);
+
+        // Request to wait until the frame time is up
+        // QUESTION: what if this time has already passed? I assume this is a noop?
+        if next_frame_time > std::time::Instant::now() {
+            *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        }
+    });
+}
+
+fn get_draw_parameters<'a>() -> glium::DrawParameters<'a> {
+    glium::DrawParameters {
+        depth: glium::Depth {
+            test: glium::draw_parameters::DepthTest::IfLess,
+            write: true,
+            ..Default::default()
+        },
+        // disabled for teapot as it has holes in it
+        //backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+        ..Default::default()
+    }
+}
+
+fn get_view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[0] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
+    ];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0f32],
+    ]
+}
+
+fn get_shader_program(display: &glium::Display) -> glium::Program {
+    let vertex_shader_src = r#"
+        #version 150
+
+        in vec3 position;
+        in vec3 normal;
+
+        out vec3 v_normal;
+        out vec3 v_position;
+
+        uniform mat4 perspective;
+        uniform mat4 view;
+        uniform mat4 model;
+
+        void main() {
+            mat4 modelview = view * model;
+            v_normal = transpose(inverse(mat3(modelview))) * normal;
+            gl_Position = perspective * modelview * vec4(position, 1.0);
+            v_position = gl_Position.xyz / gl_Position.w;
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        in vec3 v_normal;
+        in vec3 v_position;
+
+        out vec4 color;
+
+        uniform vec3 u_light;
+
+        const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
+        const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
+        const vec3 specular_color = vec3(1.0, 1.0, 1.0);
+
+        void main() {
+            float diffuse = max(dot(normalize(v_normal), normalize(u_light)), 0.0);
+
+            vec3 camera_dir = normalize(-v_position);
+            vec3 half_dir = normalize(normalize(u_light) + camera_dir);
+            float specular = pow(max(dot(half_dir, normalize(v_normal)), 0.0), 16.0);
+
+            color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);
+        }
+    "#;
+
+    glium::Program::from_source(display, vertex_shader_src, fragment_shader_src, None).unwrap()
+}
+
+fn get_uniforms(frame: &glium::Frame) -> impl glium::uniforms::Uniforms {
+    uniform! {
+        model: [
+            [0.01, 0.0, 0.0, 0.0],
+            [0.0, 0.01, 0.0, 0.0],
+            [0.0, 0.0, 0.01, 0.0],
+            [0.0, 0.0, 2.0, 1.0f32],
+        ],
+        u_light: [1.4, 0.4, -0.7f32],
+        perspective: get_perspective_matrix(frame),
+        // Virtual Camera
+        view: get_view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]),
+    }
+}
+
+type PerspectiveMatrix = [[f32; 4]; 4];
+fn get_perspective_matrix(frame: &glium::Frame) -> PerspectiveMatrix {
+    let (width, height) = frame.get_dimensions();
+    let aspect_ratio = height as f32 / width as f32;
+
+    let fov: f32 = 3.141592 / 3.0;
+    let zfar = 1024.0;
+    let znear = 0.1;
+
+    let f = 1.0 / (fov / 2.0).tan();
+
+    [
+        [f * aspect_ratio, 0.0, 0.0, 0.0],
+        [0.0, f, 0.0, 0.0],
+        [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+        [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+    ]
+}
