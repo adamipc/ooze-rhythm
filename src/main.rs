4e85feedf0fafbc7dc63edf68933b01dcb434fd7
Adam Wendt <adam@awendtconsulting.com>
2023-04-13 14:01:33 -0700

    working on presets

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,111 @@ use rand::Rng;
 use std::cell::RefCell;
 
 struct SlimeMould {
-    width: u32,
-    height: u32,
     target_texture: RefCell<glium::texture::Texture2d>,
     shader_pipeline: ShaderPipeline,
+    preset: Preset,
+}
+
+#[derive(Copy, Clone)]
+struct Preset {
+    // Initial config
+    number_of_points: u32,
+    starting_arrangement: StartingArrangement,
+    average_starting_speed: f32,
+    starting_speed_spread: f32,
+
+    // Vertex Shader Uniforms
+    speed_multiplier: f32,
+    point_size: f32,
+    random_steer_factor: f32,
+    constant_steer_factor: f32,
+    trail_strength: f32,
+    search_radius: f32,
+    wall_strategy: WallStrategy,
+    color_strategy: ColorStrategy,
+
+    // Fragment Shader Uniforms
+    fade_speed: f32,
+    blurring: f32,
+}
+
+#[derive(Copy, Clone)]
+enum StartingArrangement {
+    Origin,
+    Random,
+    Ring,
+}
+
+#[derive(Copy, Clone)]
+enum WallStrategy {
+    Wrap,
+    Bounce,
+}
+
+#[derive(Copy, Clone)]
+enum ColorStrategy {
+    Direction,
+    Speed,
+    Position,
+    Grey,
+}
+
+enum PresetName {
+    GreenSlime,
+    Tartan,
+}
+
+impl Preset {
+    pub fn new(preset_name: PresetName) -> Preset {
+        match preset_name {
+            PresetName::GreenSlime => Preset {
+                number_of_points: u32::pow(2, 18),
+                starting_arrangement: StartingArrangement::Origin,
+                average_starting_speed: 0.0,
+                starting_speed_spread: 0.3,
+
+                speed_multiplier: 1.0,
+                point_size: 1.0,
+                random_steer_factor: 0.1,
+                constant_steer_factor: 0.1,
+                trail_strength: 0.01,
+                search_radius: 0.01,
+                wall_strategy: WallStrategy::Bounce,
+                color_strategy: ColorStrategy::Position,
+
+                fade_speed: 0.01,
+                blurring: 1.0,
+            },
+            PresetName::Tartan => Preset {
+                number_of_points: u32::pow(2, 16),
+                starting_arrangement: StartingArrangement::Origin,
+                average_starting_speed: 0.8,
+                starting_speed_spread: 0.1,
+
+                speed_multiplier: 1.0,
+                point_size: 1.0,
+                random_steer_factor: 0.05,
+                constant_steer_factor: 0.01,
+                trail_strength: 0.01,
+                search_radius: 0.1,
+                wall_strategy: WallStrategy::Wrap,
+                color_strategy: ColorStrategy::Direction,
+
+                fade_speed: 0.01,
+                blurring: 1.0,
+            },
+        }
+    }
 }
 
 fn main() {
     // 1. The **winit::EventsLoop** for handling events.
     let event_loop = glutin::event_loop::EventLoop::new();
 
+    let (width, height) = (256, 256);
     // 2. Parameters for building the Window.
     let wb = glutin::window::WindowBuilder::new()
-        .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_inner_size(glutin::dpi::LogicalSize::new(width as f32, height as f32))
         .with_title("Hello world!");
 
     // 3. Parameters for building the OpenGL context.
@@ -28,7 +120,9 @@ fn main() {
     //    and register the window with the event_loop.
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let mut slime_mould = SlimeMould::new(&display, 1024, 768);
+    let preset = Preset::new(PresetName::GreenSlime);
+
+    let slime_mould = SlimeMould::new(&display, width, height, preset);
 
     // Loop forever until we receive `CloseRequested` event.
     event_loop.run(move |ev, _, control_flow| {
@@ -55,18 +149,20 @@ fn main() {
 
         // Draw stuff!
         let mut target = display.draw();
-        target.clear_color(1.0, 1.0, 1.0, 1.0);
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
         slime_mould.draw(&mut target, &display);
         target.finish().unwrap();
         // End draw stuff...
 
-        // Attempt to display 60 frames per second.
-        let next_frame_time = frame_start_time + std::time::Duration::from_nanos(16_666_667);
+        // Attempt to display 30 frames per second.
+        let next_frame_time = frame_start_time + std::time::Duration::from_nanos(16_666_667) * 4;
 
         // Request to wait until the frame time is up
         // QUESTION: what if this time has already passed? I assume this is a noop?
         if next_frame_time > std::time::Instant::now() {
             *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        } else {
+            println!("Missed frame time!");
         }
     });
 }
@@ -103,10 +199,8 @@ struct ShaderPipeline {
 }
 
 impl SlimeMould {
-    pub fn new(display: &glium::Display, width: u32, height: u32) -> Self {
+    pub fn new(display: &glium::Display, width: u32, height: u32, preset: Preset) -> Self {
         Self {
-            width,
-            height,
             target_texture: RefCell::new(
                 glium::texture::Texture2d::empty_with_format(
                     display,
@@ -117,30 +211,22 @@ impl SlimeMould {
                 )
                 .unwrap(),
             ),
-            shader_pipeline: Self::create_shader_pipeline(display, width, height),
+            shader_pipeline: Self::create_shader_pipeline(display, width, height, preset),
+            preset,
         }
     }
 
-    fn create_shader_pipeline(display: &glium::Display, width: u32, height: u32) -> ShaderPipeline {
+    fn create_shader_pipeline(
+        display: &glium::Display,
+        width: u32,
+        height: u32,
+        preset: Preset,
+    ) -> ShaderPipeline {
         // Shader 1
-        let shader_1 = Self::get_shader_1(display);
-
-        for uniform in shader_1.uniforms() {
-            println!("{:?}", uniform);
-        }
-        for attribute in shader_1.attributes() {
-            println!("{:?}", attribute);
-        }
+        let shader_1 = Self::get_shader_1(display, preset);
 
         // Shader 2
         let shader_2 = Self::get_shader_2(display);
-
-        for uniform in shader_2.uniforms() {
-            println!("{:?}", uniform);
-        }
-        for attribute in shader_2.attributes() {
-            println!("{:?}", attribute);
-        }
 
         // Textures
         let u_texture0 = glium::texture::Texture2d::empty_with_format(
@@ -180,7 +266,13 @@ impl SlimeMould {
         )
         .unwrap();
 
-        let (buffer_a, buffer_b) = Self::get_initial_locations(display, u32::pow(2, 16));
+        let (buffer_a, buffer_b) = Self::get_initial_locations(
+            display,
+            preset.number_of_points,
+            preset.starting_arrangement,
+            preset.average_starting_speed,
+            preset.starting_speed_spread,
+        );
 
         ShaderPipeline {
             buffer_a: RefCell::new(buffer_a),
@@ -213,7 +305,6 @@ impl SlimeMould {
         uniform sampler2D u_texture0; // Output of shader 1
         uniform sampler2D u_texture1; // Previous frame's output from shader 2
 
-        uniform float u_time;
         uniform float u_fade_speed;
         uniform float u_blur_fraction;
 
@@ -222,7 +313,7 @@ impl SlimeMould {
         // for blurring
         const float Directions = 8.0;
         const float Quality = 1.0; // 3 for snowflake
-        const float Radius = 1.0/12000.0; // TODO pass in resolution
+        const float Radius = 1.0/1200.0; // TODO pass in resolution
         float pixel_count = 1.0;
 
         void main() {
@@ -253,8 +344,73 @@ impl SlimeMould {
         glium::Program::from_source(display, vertex_shader_src, fragment_shader_src, None).unwrap()
     }
 
-    fn get_shader_1(display: &glium::Display) -> glium::Program {
-        let vertex_shader_src = r#"
+    fn get_shader_1(display: &glium::Display, preset: Preset) -> glium::Program {
+        let random_steer_factor = preset.random_steer_factor;
+        let constant_steer_factor = preset.constant_steer_factor;
+        let search_radius = preset.search_radius;
+        let trail_strength = preset.trail_strength;
+        let point_size = preset.point_size;
+
+        let wall_strategy_code = match preset.wall_strategy {
+            WallStrategy::Bounce => {
+                r#"
+            // reverse direction if hitting wall
+            if (y_new + speed*sin(direction) > 0.90) {
+                float d = atan(sin(direction), cos(direction));
+                direction -= 2.0*d;
+            }
+            if (y_new + speed*sin(direction) < -0.90) {
+                float d = atan(sin(direction), cos(direction));
+                direction -= 2.0*d;
+            }
+            if (x_new + speed*cos(direction) > 0.90) {
+                float d = atan(cos(direction), sin(direction));
+                direction += 2.0*d;
+            }
+            if (x_new + speed*cos(direction) < -0.90) {
+                float d = atan(cos(direction), sin(direction));
+                direction += 2.0*d;
+            }
+            "#
+            }
+            WallStrategy::Wrap => {
+                r#"
+            // wrap around
+            if (y_new > 0.99) { y_new = -0.99; }
+            if (y_new < -0.99) { y_new = 0.99; }
+
+            if (x_new > 0.99) { x_new = -0.99; }
+            if (x_new < -0.99) { x_new = 0.99; }
+            "#
+            }
+        };
+
+        let color_strategy_code = match preset.color_strategy {
+            ColorStrategy::Position => r#"
+                r = abs(y_new)/2.0 + 0.5;
+                g = abs(x_new)/2.0 + 0.5;
+            "#
+            .to_string(),
+            ColorStrategy::Direction => r#"
+                r = sin(direction);
+                g = cos(direction);
+            "#
+            .to_string(),
+            ColorStrategy::Grey => format!(
+                r#"
+                r = {trail_strength};
+                g = r;
+            "#
+            ),
+            ColorStrategy::Speed => r#"
+                r = speed_var*50.0;
+                g = r;
+            "#
+            .to_string(),
+        };
+
+        let vertex_shader_src = format!(
+            r#"
         #version 150
         precision highp float;
 
@@ -268,18 +424,18 @@ impl SlimeMould {
         out vec4 v_color;
 
         // TODO: make these uniform inputs?
-        const float random_steer_factor = 0.1;
-        const float constant_steer_factor = 0.5;
-        const float search_radius = 0.1;
+        const float random_steer_factor = {random_steer_factor};
+        const float constant_steer_factor = {constant_steer_factor};
+        const float search_radius = {search_radius};
         const float search_angle = 0.2;
-        const float trail_strength = 0.2;
-        const float vertex_radius = 1.0;
+        const float trail_strength = {trail_strength};
+        const float vertex_radius = {point_size};
 
-        float rand(vec2 co) {
+        float rand(vec2 co) {{
             return fract(sin(dot(co.xy, vec2(12.9898,78.233))) * 43758.5453);
-        }
+        }}
 
-        void main() {
+        void main() {{
             // Coord in texture space
             vec2 texcoord = vec2((a_position.x+1.0)/2.0, (a_position.y+1.0)/2.0);
             vec4 tex_val = texture2D(u_texture1, texcoord);
@@ -323,38 +479,22 @@ impl SlimeMould {
             float steer_amount = constant_steer_factor + random_steer_factor * rand(texcoord+tex_val.xy);
 
             // Straight ahead
-            if (sense_forward > sense_left && sense_forward > sense_right) {
+            if (sense_forward > sense_left && sense_forward > sense_right) {{
                 direction += 0.0;
-            } else if (sense_forward < sense_left && sense_forward < sense_right) { // random
+            }} else if (sense_forward < sense_left && sense_forward < sense_right) {{ // random
                 direction += random_steer_factor*(rand(texcoord+tex_val.xy)-0.5);
-            } else if (sense_right > sense_left) {
+            }} else if (sense_right > sense_left) {{
                 direction -= steer_amount; // Turn right
-            } else if (sense_right < sense_left) {
+            }} else if (sense_right < sense_left) {{
                 direction += steer_amount; // Turn left
-            }
+            }}
 
             // Start calculating our new position
-            float y_new = a_position.x;
+            float y_new = a_position.y;
             float x_new = a_position.x;
 
-            // Hard coded bounce handling at edges
-            // reverse direction if hitting wall
-            if (y_new + speed*sin(direction) > 0.90) {
-                float d = atan(sin(direction), cos(direction));
-                direction -= 2.0*d;
-            }
-            if (y_new + speed*sin(direction) < -0.90) {
-                float d = atan(sin(direction), cos(direction));
-                direction -= 2.0*d;
-            }
-            if (x_new + speed*cos(direction) > 0.90) {
-                float d = atan(cos(direction), sin(direction));
-                direction += 2.0*d;
-            }
-            if (x_new + speed*cos(direction) < -0.90) {
-                float d = atan(cos(direction), sin(direction));
-                direction += 2.0*d;
-            }
+            // Wall strategy
+            {wall_strategy_code}
 
             // Update position based on direction
             y_new += speed*speed_multiplier*sin(direction);
@@ -364,23 +504,23 @@ impl SlimeMould {
             float r = 0.0;
             float g = 0.0;
 
-            // hard coded color strategy for direction
-            r = sin(direction);
-            g = cos(direction);
+            // Color strategy
+            {color_strategy_code}
 
             v_color = vec4(r, g, trail_strength, 1.0);
 
             // Send back the position and size
             gl_Position = vec4(x_new, y_new, speed_var/1000.0, 1.0+direction/1000.0);
             gl_PointSize = vertex_radius;
-        }
-    "#;
+        }}
+    "#
+        );
 
         let fragment_shader_src = r#"
         #version 150
         precision highp float;
 
-        out vec4 v_color;
+        in vec4 v_color;
 
         void main() {
             gl_FragColor = v_color;
@@ -390,7 +530,7 @@ impl SlimeMould {
         glium::Program::new(
             display,
             glium::program::ProgramCreationInput::SourceCode {
-                vertex_shader: vertex_shader_src,
+                vertex_shader: &vertex_shader_src[..],
                 fragment_shader: fragment_shader_src,
                 geometry_shader: None,
                 tessellation_control_shader: None,
@@ -409,21 +549,44 @@ impl SlimeMould {
     fn get_initial_locations(
         display: &glium::Display,
         n: u32,
+        starting_arrangement: StartingArrangement,
+        initial_speed: f32,
+        speed_randomness: f32,
     ) -> (glium::VertexBuffer<Position>, glium::VertexBuffer<Position>) {
         let mut initial_locations = vec![Position::default(); n as usize];
-        let initial_speed = 1.0;
-        let speed_randomness = 0.1;
 
         let mut rng = rand::thread_rng();
         for i in 0..n {
-            let x: f32 = rng.gen_range(-1.0..1.0);
-            let y: f32 = rng.gen_range(-1.0..1.0);
-            let speed: f32 = (rng.gen_range(0.0..1.00) * 0.01 * speed_randomness
-                + 0.01 * initial_speed)
+            let speed = (rng.gen_range(0.0..1.00) * 0.01 * speed_randomness + 0.01 * initial_speed)
                 / 1000.0;
-            let direction: f32 = rng.gen_range(0.0..1.0);
             initial_locations[i as usize] = Position {
-                a_position: [x, y, speed, direction],
+                a_position: match starting_arrangement {
+                    StartingArrangement::Random => [
+                        rng.gen_range(-1.0..1.0), // x
+                        rng.gen_range(-1.0..1.0), // y
+                        speed,                    // speed
+                        rng.gen_range(0.0..1.0),  // direction
+                    ],
+                    StartingArrangement::Ring => {
+                        let a = i as f32 * std::f32::consts::PI * 2.0 / (n as f32 * 4.0); // angle
+                        let d = 0.7; // distance from center
+                        [
+                            a.sin() * d,                                      // x
+                            -a.cos() * d,                                     // y
+                            speed,                                            // speed
+                            1.0 + (a + std::f32::consts::FRAC_PI_2) / 1000.0, // direction
+                        ]
+                    }
+                    StartingArrangement::Origin => {
+                        let a = i as f32 * std::f32::consts::PI * 2.0 / (n as f32 * 4.0); // angle
+                        [
+                            0.0,
+                            0.0,
+                            speed,
+                            1.0 + (a + std::f32::consts::FRAC_PI_2) / 1000.0,
+                        ]
+                    }
+                },
             };
         }
 
@@ -456,6 +619,10 @@ impl SlimeMould {
         }
 
         self.draw_1(frame, display);
+
+        self.shader_pipeline
+            .buffer_a
+            .swap(&self.shader_pipeline.buffer_b);
 
         {
             let target_texture = self.target_texture.borrow();
@@ -492,7 +659,8 @@ impl SlimeMould {
             let draw_parameters = Self::get_draw_parameters_shader_1(&session);
 
             let u_texture1 = &*self.shader_pipeline.u_texture1.borrow();
-            let uniforms = uniform! { u_texture1: u_texture1, speed_multiplier: 1.0f32 };
+            let uniforms =
+                uniform! { u_texture1: u_texture1, speed_multiplier: self.preset.speed_multiplier };
 
             // Draw shader_1 to the frame
             frame
@@ -505,21 +673,16 @@ impl SlimeMould {
                 )
                 .unwrap();
         }
-        // Swap buffers
-        std::mem::swap(
-            &mut self.shader_pipeline.buffer_a.borrow_mut(),
-            &mut self.shader_pipeline.buffer_b.borrow_mut(),
-        );
     }
 
-    pub fn draw_2(&self, frame: &mut impl glium::Surface, display: &glium::Display) {
+    pub fn draw_2(&self, frame: &mut impl glium::Surface, _display: &glium::Display) {
         let u_texture0 = &*self.shader_pipeline.u_texture0.borrow();
         let u_texture1 = &*self.shader_pipeline.u_texture1.borrow();
         let uniforms = uniform! {
             u_texture0: u_texture0,
             u_texture1: u_texture1,
-            u_fade_speed: 0.07f32,
-            u_blur_fraction: 1.0f32,
+            u_fade_speed: self.preset.fade_speed,
+            u_blur_fraction: self.preset.blurring,
         };
         // Draw the results of shader_2 to the screen
         frame
